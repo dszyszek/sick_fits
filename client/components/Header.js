6ff61f9d5e0e5d9c2831704d9409721cb9b8548d
Damian <37978110+dszyszek@users.noreply.github.com>
2019-07-22 00:47:24 +0200

    Add Header component

---
+++ client/components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => (
+    <div>
+        <div class='bar'>
+            <a href=''>Sick Fits</a>
+        </div>
+
+        <div class='sub-bar'>
+            <p>Search</p>
+        </div>
+
+        <div>Cart</div>
+
+    </div>
+);
+
+export default Header;
