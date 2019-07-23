4353c6b59c3497b95111711d4e4297b4932b3b14
Damian <37978110+dszyszek@users.noreply.github.com>
2019-07-24 00:31:30 +0200

    Add methods to resolvers

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,3 +1,8 @@
-const Query = {};
+const Query = {
+    async items(paremt, args, ctx, info) {
+        const items = await ctx.db.query.item();
+        return items;
+    }
+};
 
 module.exports = Query;
