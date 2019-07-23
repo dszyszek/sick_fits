047083ec7866727971fcf21c392766a433bfc6bb
Damian <37978110+dszyszek@users.noreply.github.com>
2019-07-23 22:32:52 +0200

    Add createServer.js file

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,17 @@
+require('dotenv').config({path: 'variables.env'});
 
+const createServer = require('./createServer');
+const db = require('./db');
+
+const serevr = createServer();
+
+
+
+serevr.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    }
+}, info => {
+    console.log(`Server is running on port ${info.port}`);
+});
