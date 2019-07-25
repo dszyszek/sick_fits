6719e9705837d2f012f3f4fa69f4157aa1b3a5af
Damian <37978110+dszyszek@users.noreply.github.com>
2019-07-25 22:25:24 +0200

    Add Item component

---
+++ client/components/Item.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Item extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+Item.propTypes = {
+
+};
+
+export default Item;
