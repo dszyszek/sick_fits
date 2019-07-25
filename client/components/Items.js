687322d300ace87d6874180536c88929dcdb5d5f
Damian <37978110+dszyszek@users.noreply.github.com>
2019-07-25 22:25:03 +0200

    Add styled divs

--- client/components/Items.js
+++ client/components/Items.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Query} from 'react-apollo';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
 
 
 const ALL_ITEMS_QUERY = gql`
@@ -16,15 +17,30 @@ const ALL_ITEMS_QUERY = gql`
     }
 `;
 
+const Center = styled.div`
+    text-align: center;
+`;
+
+const ItemsList = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 60px;
+    max-width: ${props => props.theme.maxWidth};
+    margin: 0 auto;
+`;
+
 class Items extends Component {
     render() {
         return (
             <div>
                 <p>Items!</p>
                 <Query query={ALL_ITEMS_QUERY}>
-                    {payload => {
-                        console.log(payload);
-                        return <p>Child of a query</p>
+                    {({data, error, loading}) => {
+                        if (loading) return <p>Loading...</p>
+                        if (error) return <p>Error... {error.message}</p>
+                        return <ItemsList>
+                            {data.items.map(item => <p>{item.title}</p>)}
+                        </ItemsList>
                     }}
                 </Query>
 
