b1d2bc036050fc72ef6793c08ff7454b060559b0
Damian <37978110+dszyszek@users.noreply.github.com>
2019-07-25 22:49:09 +0200

    Handle query strings (Item component)

--- client/components/Item.js
+++ client/components/Item.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
+
+import Title from './styles/Title';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+
 
 class Item extends Component {
     render() {
+        const {item} = this.props;
+
         return (
-            <div>
-                
-            </div>
+            <ItemStyles>
+                <Title>
+                    <Link href={{
+                        pathname: '/item',
+                        query: {id: item.id}
+                    }}>
+                        <a>{item.title}</a>
+                    </Link>
+
+                </Title>
+            </ItemStyles>
         );
     }
 }
 
 Item.propTypes = {
-
+    item: PropTypes.object.isRequired
 };
 
 export default Item;
--- client/components/Items.js
+++ client/components/Items.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Query} from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import Item from './Item';
 
 
 const ALL_ITEMS_QUERY = gql`
@@ -32,19 +33,19 @@ const ItemsList = styled.div`
 class Items extends Component {
     render() {
         return (
-            <div>
+            <Center>
                 <p>Items!</p>
                 <Query query={ALL_ITEMS_QUERY}>
                     {({data, error, loading}) => {
                         if (loading) return <p>Loading...</p>
                         if (error) return <p>Error... {error.message}</p>
                         return <ItemsList>
-                            {data.items.map(item => <p>{item.title}</p>)}
+                            {data.items.map(item => <Item key={item.id} item={item}>{item.title}</Item>)}
                         </ItemsList>
                     }}
                 </Query>
 
-            </div>
+            </Center>
         );
     }
 }
