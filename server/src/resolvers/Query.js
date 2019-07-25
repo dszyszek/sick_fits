7e78f7b166da69a75f7e6d20927c32bc13b58230
Damian <37978110+dszyszek@users.noreply.github.com>
2019-07-25 21:29:22 +0200

    Fix errors with createServer

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,6 +1,6 @@
 const Query = {
     async items(paremt, args, ctx, info) {
-        const items = await ctx.db.query.item();
+        const items = await ctx.db.query.items();
         return items;
     }
 };
