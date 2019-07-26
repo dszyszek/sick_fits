081484e51444677fa192b063ee38f24afd9833a7
Damian <37978110+dszyszek@users.noreply.github.com>
2019-07-26 04:53:37 +0200

    Allow adding images and its preview

--- client/components/CreateItem.js
+++ client/components/CreateItem.js
@@ -45,6 +45,26 @@ class CreateItem extends Component {
         });
     } 
 
+    uploadFile = async e => {
+        console.log('uploading file...');
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sickfits');
+
+        const res = await fetch('https://api.cloudinary.com/v1_1/dszyszek/image/upload', {
+            method: 'POST',
+            body: data
+        });
+
+        const file = await res.json();
+
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url
+        });
+    }
+
     render() {
         return (
             <Mutation 
@@ -63,44 +83,60 @@ class CreateItem extends Component {
                     }}>
                         <Error error={error} />
                         <fieldset disabled={loading} aria-busy={loading}>
-                            <label htmlFor='title'>
-                            Title
+                            <label htmlFor='file'>
+                                Image
                             
-                            <input 
-                            type='text' 
-                            id='title' 
-                            name='title' 
-                            placeholder='Title' 
-                            required 
-                            value={this.state.title} 
-                            onChange={this.updateValue}
-                            />
+                                <input 
+                                type='file' 
+                                id='file' 
+                                name='file' 
+                                placeholder='Upload an image' 
+                                required 
+                                onChange={this.uploadFile}
+                                />
+
+                                {this.state.image && <img src={this.state.image} alt='Upload preview' />}
+                            </label>
+                        
+                            <label htmlFor='title'>
+                                Title
+                                
+                                <input 
+                                type='text' 
+                                id='title' 
+                                name='title' 
+                                placeholder='Title' 
+                                required 
+                                value={this.state.title} 
+                                onChange={this.updateValue}
+                                />
                             </label>
                             
                             <label htmlFor='price'>
-                            Price
-                            <input 
-                            type='number' 
-                            id='price' 
-                            name='price' 
-                            placeholder='Price' 
-                            required 
-                            value={this.state.price} 
-                            onChange={this.updateValue}
-                            />
-                            
+                                Price
+                                <input 
+                                type='number' 
+                                id='price' 
+                                name='price' 
+                                placeholder='Price' 
+                                required 
+                                value={this.state.price} 
+                                onChange={this.updateValue}
+                                />
+                                
                             </label>
                             
                             <label htmlFor='description'>
-                            Description   
-                            <textarea 
-                            id='description' 
-                            name='description' 
-                            placeholder='Type in some description' 
-                            required 
-                                    value={this.state.description} 
-                                    onChange={this.updateValue}
-                                    />
+                                Description   
+                                <textarea 
+                                id='description' 
+                                name='description' 
+                                placeholder='Type in some description' 
+                                required 
+                                        value={this.state.description} 
+                                        onChange={this.updateValue}
+                                />
+
                             </label>
                             <button type='submit'>Submit</button>
                         
