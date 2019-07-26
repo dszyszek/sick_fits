4781e6178041b5a8745fe29a10dbf5a1adf228a3
Damian <37978110+dszyszek@users.noreply.github.com>
2019-07-26 03:41:20 +0200

    Make CreateItem component functioning (making queries, handle loading etc.)

--- client/components/CreateItem.js
+++ client/components/CreateItem.js
@@ -4,15 +4,110 @@ import gql from 'graphql-tag';
 
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
+import Error from './ErrorMessage';
+
+
+const CREATE_ITEM_MUTATION = gql`
+    mutation CREATE_ITEM_MUTATION(
+        $title: String!
+        $description: String!
+        $price: Int!
+        $image: String
+        $largeImage: String
+    ) {
+        createItem(
+            title: $title
+            description: $description
+            price: $price
+            image: $image
+            largeImage: $largeImage
+        ) {
+            id
+        }
+    }
+`;
 
 class CreateItem extends Component {
+    state = {
+        title: '',
+        description: '',
+        image: '',
+        largeImage: '',
+        price: ''
+    };
+
+    updateValue = (e) => {
+        const {name, type, value} = e.target;
+        const val = type === 'number' ? parseFloat(value) : value;
+        this.setState({
+            [name]: val
+        });
+    } 
+
     render() {
         return (
-            <Form>
-                <h2>Sell an item.</h2>
-            </Form>
-        );
-    }
-}
+            <Mutation 
+                mutation={CREATE_ITEM_MUTATION}
+                variables={this.state}
+            >
+                {(createItem, {loading, error}) => (
 
-export default CreateItem;
+                    <Form onSubmit={async e => {
+                        e.preventDefault();
+                        const res = await createItem();
+                        
+                    }}>
+                        <Error error={error} />
+                        <fieldset disabled={loading} aria-busy={loading}>
+                            <label htmlFor='title'>
+                            Title
+                            
+                            <input 
+                            type='text' 
+                            id='title' 
+                            name='title' 
+                            placeholder='Title' 
+                            required 
+                            value={this.state.title} 
+                            onChange={this.updateValue}
+                            />
+                            </label>
+                            
+                            <label htmlFor='price'>
+                            Price
+                            <input 
+                            type='number' 
+                            id='price' 
+                            name='price' 
+                            placeholder='Price' 
+                            required 
+                            value={this.state.price} 
+                            onChange={this.updateValue}
+                            />
+                            
+                            </label>
+                            
+                            <label htmlFor='description'>
+                            Description   
+                            <textarea 
+                            id='description' 
+                            name='description' 
+                            placeholder='Type in some description' 
+                            required 
+                                    value={this.state.description} 
+                                    onChange={this.updateValue}
+                                    />
+                            </label>
+                            <button type='submit'>Submit</button>
+                        
+                        </fieldset>
+                    </Form>
+                )}
+            </Mutation>
+                            );
+                        }
+                    }
+                    
+                    export default CreateItem;
+                    export {CREATE_ITEM_MUTATION};
+                    
