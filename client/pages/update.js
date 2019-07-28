75a334c9c13fbede0ae081d802e3bec014cb7014
Damian <37978110+dszyszek@users.noreply.github.com>
2019-07-28 15:46:07 +0200

    Add update page

---
+++ client/pages/update.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import UpdateItem from '../components/CreateItem';
+
+const Update = props => (
+    <div>
+        <UpdateItem />
+    </div>
+);
+
+export default Update;
