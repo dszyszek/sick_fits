01578f7362c65f1791ba67b786a5ba47b0dec36c
Damian <37978110+dszyszek@users.noreply.github.com>
2019-07-28 21:55:06 +0200

    Add DeleteButton (Delete component)

---
+++ client/components/Delete.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Delete extends Component {
+    render() {
+        return (
+            <button> Delete item </button>
+        );
+    }
+}
+
+export default Delete;
--- client/components/Item.js
+++ client/components/Item.js
@@ -6,6 +6,7 @@ import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
+import DeleteButton from './Delete';
 
 class Item extends Component {
     render() {
@@ -37,7 +38,7 @@ class Item extends Component {
                         <a>Edit</a>
                     </Link>
                     <button>Add to cart</button>
-                    <button>Delete</button>
+                    <DeleteButton />
                 </div>
             </ItemStyles>
         );
