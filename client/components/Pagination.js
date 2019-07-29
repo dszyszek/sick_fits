6ab1d3748d28fa7c9208a2715e41f9588e561565
Damian <37978110+dszyszek@users.noreply.github.com>
2019-07-29 02:41:11 +0200

    Add Pagination component

---
+++ client/components/Pagination.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import PaginationStyles from './styles/PaginationStyles';
+
+
+const Pagination = props => {
+    <PaginationStyles>
+        <p>_PAGINATION_</p>
+    </PaginationStyles>
+};
+
+export default Pagination;
