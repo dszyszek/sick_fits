a59dbd04498349e723047b76a1aa5b4268f73c89
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-01 07:00:29 +0200

    Create Signup component

---
+++ client/components/Signup.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {Mutation} from 'react-apollo';
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import Error from './ErrorMessage';
+
+
+class Signup extends Component {
+    render() {
+        return (
+            <div>
+                <h1>/signup</h1>
+            </div>
+        );
+    }
+}
+
+export default Signup;
