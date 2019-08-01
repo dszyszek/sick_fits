136dc3de19b8d0d11042713d519dbfc0b2b25c4f
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-01 23:56:28 +0200

    Make columns

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -6,11 +6,41 @@ import Error from './ErrorMessage';
 
 
 class Signup extends Component {
+    state = {
+        name: '',
+        password: '',
+        email: ''
+    }
+
+    saveToState = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
     render() {
         return (
-            <div>
-                <h1>/signup</h1>
-            </div>
+            <Form>
+                <fieldset>
+                    <h2>Sign up for an account</h2>
+                    <label for='email'>
+                        Email
+                        <input type='email' name='email' placeholder='email' value={this.state.email} onChange={this.saveToState} />
+                    </label>
+
+                    <label for='name'>
+                        Name
+                        <input type='text' name='name' placeholder='name' value={this.state.name} onChange={this.saveToState} />
+                    </label>
+
+                    <label for='password'>
+                        Password
+                        <input type='password' name='password' placeholder='password' value={this.state.password} onChange={this.saveToState} />
+                    </label>
+                    <button type='submit'>Sign up!</button>
+                </fieldset>
+            
+            </Form>
         );
     }
 }
--- client/pages/signup.js
+++ client/pages/signup.js
@@ -1,9 +1,19 @@
 import Signup from '../components/Signup';
+import styled from 'styled-components';
 
-const signup = props => (
-    <div>
-        <Signup />
-    </div>
+
+const Columns = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-gap: 20px;
+`;
+
+const SignupPage = props => (
+        <Columns>
+            <Signup />
+            <Signup />
+            <Signup />
+        </Columns>
 );
 
-export default signup;
+export default SignupPage;
