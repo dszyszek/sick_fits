f49b097b0177a070deef7c8ffb0196e284c72ed5
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-02 20:39:48 +0200

    Complete sign-up process (Signup component)

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -5,6 +5,16 @@ import Form from './styles/Form';
 import Error from './ErrorMessage';
 
 
+const SIGNUP_MUTATION = gql`
+    mutation SIGNUP_MUTATION($name: String!, $email: String!, $password: String!) {
+        signup(name: $name, email: $email, password: $password) {
+            id
+            email
+            name
+        }
+    }
+`;
+
 class Signup extends Component {
     state = {
         name: '',
@@ -20,27 +30,49 @@ class Signup extends Component {
 
     render() {
         return (
-            <Form>
-                <fieldset>
-                    <h2>Sign up for an account</h2>
-                    <label for='email'>
-                        Email
-                        <input type='email' name='email' placeholder='email' value={this.state.email} onChange={this.saveToState} />
-                    </label>
+            <Mutation 
+                mutation={SIGNUP_MUTATION} 
+                variables={this.state} 
+            >
+                {(signup, {error, loading}) => {
+                    return (
 
-                    <label for='name'>
-                        Name
-                        <input type='text' name='name' placeholder='name' value={this.state.name} onChange={this.saveToState} />
-                    </label>
+                        <Form 
+                            method='POST' 
+                            onSubmit={async e => {
+                                e.preventDefault();
+                                const res = await signup();
+                                console.log(res);
+                                this.setState({name: '', email: '', password: ''});
+                            }}
+                        >
+                            <fieldset disabled={loading} aria-busy={loading}>
+                                <h2>Sign up for an account</h2>
+                                <Error error={error} />
 
-                    <label for='password'>
-                        Password
-                        <input type='password' name='password' placeholder='password' value={this.state.password} onChange={this.saveToState} />
-                    </label>
-                    <button type='submit'>Sign up!</button>
-                </fieldset>
-            
-            </Form>
+                                <label htmlFor='email'>
+                                    Email
+                                    <input type='email' name='email' placeholder='email' value={this.state.email} onChange={this.saveToState} />
+                                </label>
+                                
+                                <label htmlFor='name'>
+                                    Name
+                                    <input type='text' name='name' placeholder='name' value={this.state.name} onChange={this.saveToState} />
+                                </label>
+                                
+                                <label htmlFor='password'>
+                                    Password
+                                    <input type='password' name='password' placeholder='password' value={this.state.password} onChange={this.saveToState} />
+                                </label>
+                                <button type='submit'>Sign up!</button>
+                            </fieldset>
+                        
+                        </Form>
+                    );
+                }}
+
+            </Mutation>
+
         );
     }
 }
