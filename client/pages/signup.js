c44173605b92856a30cd312d227ca5f55bb3c02b
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-03 03:37:03 +0200

    Create Signin component and use it in /signup

--- client/pages/signup.js
+++ client/pages/signup.js
@@ -1,4 +1,5 @@
 import Signup from '../components/Signup';
+import Signin from '../components/Signin';
 import styled from 'styled-components';
 
 
@@ -11,7 +12,7 @@ const Columns = styled.div`
 const SignupPage = props => (
         <Columns>
             <Signup />
-            <Signup />
+            <Signin />
             <Signup />
         </Columns>
 );
