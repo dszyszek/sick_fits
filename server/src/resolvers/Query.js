ab48e3d1a500c76f147fc76cdb69668991077256
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-04 23:39:37 +0200

    Add users query and its resolver

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,9 +1,12 @@
 const {forwardTo} = require('prisma-binding');
+const {hasPermissions} = require('../utils');
+
 
 const Query = {
     items: forwardTo('db'),
     item: forwardTo('db'),
     itemsConnection: forwardTo('db'),
+    
     me(parent, args, ctx, info) {
         if (!ctx.request.userId) {
             return null;
@@ -12,6 +15,16 @@ const Query = {
         return ctx.db.query.user({
             where: {id: ctx.request.userId}
         }, info);
+    },
+
+    users(parent, args, ctx, info) {
+        if (!ctx.request.userId) {
+            throw new Error('You must be logged in to proceed!');
+        }
+
+        hasPermissions(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+
+        return ctx.db.users({}, info);
     }
 };
 
