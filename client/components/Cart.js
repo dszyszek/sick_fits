46f79abcc7557dcb605b8e95252ca3fb443d80a2
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-09 17:14:53 +0200

    Add Cart component

---
+++ client/components/Cart.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import CartStyles from './styles/CartStyles'
+import Supreme from './styles/Supreme'
+import CloseButton from './styles/CloseButton'
+import SickButton from './styles/SickButton'
+
+
+class Cart extends Component {
+    render() {
+        return (
+            <div>
+                Cart
+            </div>
+        );
+    }
+}
+
+export default Cart;
