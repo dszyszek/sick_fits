6bba693be24fb78e79fd588b19f3ffe37a55ed10
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-18 00:59:01 +0200

    Add addToCart mutation and its resolver

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -186,6 +186,34 @@ const mutations = {
                 id: args.userId
             },
         }, info);
+    },
+
+    async addToCart(parent, args, ctx, info) {
+        const {userId} = ctx.request;
+
+        if (!userId) {
+            throw new Error('You must be logged in!');
+        }
+
+        const [existingCartItem] = ctx.db.query.cartItems({
+            user: {id: userId},
+            item: {id: args.id}
+        });
+        
+        if (existingCartItem) {
+            throw new Error('This item is already in the cart');
+        }
+
+        return ctx.db.mutation.createCartItem({
+            data: {
+                user: {
+                    connect: {id: userId}
+                },
+                item: {
+                    connect: {id: args.id}
+                }
+            }
+        });
     }
 
 };
