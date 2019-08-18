ea59497382d836273068de95a52844cee4214cda
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-19 01:58:10 +0200

    Add removeFromCart schema and its resolver

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -223,6 +223,24 @@ const mutations = {
                 }
             }
         }, info);
+    },
+
+    async removeFromCart(parent, args, ctx, info) {
+        const cartItem = await ctx.db.query.cartItem({
+            where: {
+                id: args.id
+            }
+        }, `{id, user {id}}`);
+
+        if (!cartItem) throw new Error('No cart item found!');
+
+        if (cartItem.user.id !== ctx.request.userId) {
+            throw new Error('Do not cheat!');
+        }
+
+        return ctx.db.mutation.deleteCartItem({
+            where: {id: args.id}
+        }, info);
     }
 
 };
