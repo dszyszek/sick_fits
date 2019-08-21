51d237e7139c63fba7b1b474ddc0ae2c2e18aa16
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-21 23:40:05 +0200

    Create CartCount component

---
+++ client/components/CartCount.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const CartCount = (props) => {
+    return (
+        <div>
+            Count
+        </div>
+    );
+};
+
+export default CartCount;
