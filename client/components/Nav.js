cef5a89572a7a83eb9d68c0093f01f95350a9054
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-21 23:50:38 +0200

    Show count of items in cart

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -5,6 +5,8 @@ import NavStyles from './styles/NavStyles'
 import User from './User';
 import Signout from './Signout';
 import {TOGGLE_CART_MUTATION} from './Cart';
+import CartCount from './CartCount';
+
 
 const Nav = props => (
     <User>
@@ -33,7 +35,10 @@ const Nav = props => (
 
                         <Mutation mutation={TOGGLE_CART_MUTATION}>
                             {(toggleCart) => (
-                                <button onClick={toggleCart}>My cart</button>
+                                <button onClick={toggleCart}>
+                                    My cart
+                                    <CartCount count={me.cart.reduce((prev, curr) => prev + curr.quantity, 0)} />
+                                </button>
                             )}
                         
                         </Mutation>
