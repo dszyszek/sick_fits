6c04858335b8a2b61d7825d5d2167e08af136af1
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-26 00:23:02 +0200

    Get credit card transaction token in client

--- client/components/GetMoney.js
+++ client/components/GetMoney.js
@@ -10,11 +10,28 @@ import Error from './ErrorMessage';
 import User, {CURRENT_USER_QUERY} from './User';
 
 class GetMoney extends React.Component {
+    totalItems = (cart) => {
+        return cart.reduce((current, next) => current + next.quantity, 0);
+    }
+
+    onToken = (res) => {
+        console.log(res);
+    }
+
     render() {
         return (
             <User>
                 {({data: {me}}) => (
-                    <StripeCheckout>
+                    <StripeCheckout
+                        amount={calcTotalPrice(me.cart)}
+                        name='Sick fits'
+                        description={`Order of ${this.totalItems(me.cart)} items!`}
+                        image={me.cart[0].item && me.cart[0].item.image}
+                        stripeKey='pk_test_zNX8GHalH77LM7hKwSZMGj1D00XSHZBPry'
+                        currency='USD'
+                        email={me.email}
+                        token={res => this.onToken(res)}
+                    >
                         {this.props.children}
                     </StripeCheckout>
                 )}
