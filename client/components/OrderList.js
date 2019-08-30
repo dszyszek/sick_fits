036ddd643cc1b44919784e8fb97c92c8ff678273
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-31 01:39:09 +0200

    Create OrderList component

---
+++ client/components/OrderList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class OrderList extends Component {
+    render() {
+        return (
+            <div>
+                Order list here.
+            </div>
+        );
+    }
+}
+
+export default OrderList;
