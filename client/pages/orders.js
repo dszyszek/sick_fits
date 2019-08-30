c3a21cb5a9df623928e71a10d9909d6d8db0ac1d
Damian <37978110+dszyszek@users.noreply.github.com>
2019-08-31 01:38:36 +0200

    Create /orders page

---
+++ client/pages/orders.js
@@ -0,0 +1,13 @@
+import RequireSignIn from '../components/RequireSignIn';
+import OrderList from '../components/OrderList';
+
+
+const OrdersPage = props => (
+    <div>
+        <RequireSignIn>
+            <OrderList />
+        </RequireSignIn>
+    </div>
+);
+
+export default OrdersPage;
